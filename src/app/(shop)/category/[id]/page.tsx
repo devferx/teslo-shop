57377539915d7e340b8f893c69617a8d39f3ecbb
devferx <ferkki@hotmail.com>
2024-03-06 18:39:07 -0400

    Add not-found category page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,21 @@
-export default function CategoryPage() {
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+
+  if (id === 'man') {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category {id} Page</h1>
     </div>
   );
 }
