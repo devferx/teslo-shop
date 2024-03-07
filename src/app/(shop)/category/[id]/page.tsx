e1c069e1a55dc9c803a35ec1ced5ecb033953324
devferx <ferkki@hotmail.com>
2024-03-07 10:09:12 -0400

    Add productGrid to CategoryPage

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,41 @@
 import { notFound } from 'next/navigation'
 
+import { ProductGrid, Title } from '@/components'
+import { Categories } from '@/interfaces'
+import { initialData } from '@/seed/seed'
+
+const seedProducts = initialData.products
+
 interface Props {
   params: {
-    id: string
+    id: Categories
   }
 }
 
 export default function CategoryPage({ params }: Props) {
   const { id } = params
+  const products = seedProducts.filter((product) => product.gender === id)
 
-  if (id === 'man') {
+  const labels: Record<Categories, string> = {
+    men: 'hombres',
+    women: 'mujeres',
+    kid: 'niños',
+    unisex: 'todos',
+  }
+
+  const lablesArray = Object.keys(labels)
+  if (!lablesArray.includes(id)) {
     notFound()
   }
 
   return (
     <div>
-      <h1>Category {id} Page</h1>
+      <Title
+        className="mb-2"
+        title={`Articulos para ${labels[id]}`}
+        subtitle="Todos los productos"
+      />
+      <ProductGrid products={products} />
     </div>
   )
 }
