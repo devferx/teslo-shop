4e6fe56633f33df031cf4c50a3cad9dccc4124a4
devferx <ferkki@hotmail.com>
2024-03-07 11:38:36 -0400

    Add Swiper component for product slideshow

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from 'next/navigation'
 
 import { titleFont } from '@/config/fonts'
-import { QuantitySelector, SizeSelector } from '@/components'
+import { ProductSlideshow, QuantitySelector, SizeSelector } from '@/components'
 import { initialData } from '@/seed/seed'
 
 interface Props {
@@ -21,7 +21,9 @@ export default function ProductPage({ params }: Props) {
   return (
     <div className="mb-20 mt-5 grid gap-3 md:grid-cols-3">
       {/* SlideShow */}
-      <div className="col-span-1 md:col-span-2">SlideShow</div>
+      <div className="col-span-1 md:col-span-2">
+        <ProductSlideshow title={product.title} images={product.images} />
+      </div>
 
       {/* Content */}
       <div className="col-span-1 px-5">
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
 export * from './products/product-grid/ProductGrid'
 export * from './products/product-grid/ProductGridItem'
 
-export * from './product/size-selector/SizeSelector'
 export * from './product/quantity-selector/QuantitySelector'
+export * from './product/size-selector/SizeSelector'
+export * from './product/slideshow/ProductSlideshow'
 
 export * from './ui/sidebar/Sidebar'
 export * from './ui/not-found/PageNotFound'
