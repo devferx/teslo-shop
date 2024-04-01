99f442ae93995093fd43516c78daaa243393ddba
devferx <ferkki@hotmail.com>
2024-04-01 11:44:55 -0400

    Add StockLabel loading state and sleep utility function

--- src/components/product/stock-label/StockLabel.tsx
+++ src/components/product/stock-label/StockLabel.tsx
@@ -11,21 +11,33 @@ interface Props {
 
 export const StockLabel = ({ slug }: Props) => {
   const [stock, setStock] = useState(0)
-  // const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   const getStock = useCallback(async () => {
     const stock = await getStockBySlug(slug)
-
     setStock(stock)
+    setIsLoading(false)
   }, [slug])
 
   useEffect(() => {
     getStock()
   }, [getStock])
 
+  if (isLoading) {
+    return (
+      <h1
+        className={`${titleFont.className} animate-pulse bg-gray-300 text-lg font-bold antialiased`}
+      >
+        &nbsp;
+      </h1>
+    )
+  }
+
   return (
-    <h1 className={`${titleFont.className} text-lg font-bold antialiased`}>
-      Stock: {stock}
-    </h1>
+    <>
+      <h1 className={`${titleFont.className} text-lg font-bold antialiased`}>
+        Stock: {stock}
+      </h1>
+    </>
   )
 }
