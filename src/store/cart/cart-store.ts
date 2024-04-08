cf15f264d42d2baddba372445e2d518d625f82a6
devferx <ferkki@hotmail.com>
2024-04-08 12:08:57 -0400

    Refactor useCartStore hook to include persist middleware in cart-store.ts

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,5 +1,6 @@
 import { CartProduct } from '@/interfaces'
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface State {
   cart: CartProduct[]
@@ -9,26 +10,34 @@ interface State {
   // removeProduct
 }
 
-export const useCartStore = create<State>()((set, get) => ({
-  cart: [],
-  // Methods
-  addProductToCart: (product) => {
-    const { cart } = get()
+export const useCartStore = create<State>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+      // Methods
+      addProductToCart: (product) => {
+        const { cart } = get()
 
-    const productInCart = cart.some(
-      (item) => item.id === product.id && item.size === product.size,
-    )
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size,
+        )
 
-    if (!productInCart) {
-      set({ cart: [...cart, product] })
-      return
-    }
+        if (!productInCart) {
+          set({ cart: [...cart, product] })
+          return
+        }
 
-    const updatedCartProducts = cart.map((item) =>
-      item.id === product.id && item.size === product.size
-        ? { ...item, quantity: item.quantity + product.quantity }
-        : item,
-    )
-    set({ cart: updatedCartProducts })
-  },
-}))
+        const updatedCartProducts = cart.map((item) =>
+          item.id === product.id && item.size === product.size
+            ? { ...item, quantity: item.quantity + product.quantity }
+            : item,
+        )
+        set({ cart: updatedCartProducts })
+      },
+    }),
+    {
+      name: 'shopping-cart',
+      // skipHydration: true,
+    },
+  ),
+)
