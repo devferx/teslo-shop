5535a276135c2ced915ff3c05fc000e908960a07
devferx <ferkki@hotmail.com>
2024-04-08 12:00:17 -0400

    Implement addProductToCart function in cart-store.ts

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -4,11 +4,31 @@ import { create } from 'zustand'
 interface State {
   cart: CartProduct[]
 
-  // addProductToCart
+  addProductToCart: (product: CartProduct) => void
   // updateProductQuantity
   // removeProduct
 }
 
-export const useCartStore = create<State>()((set) => ({
+export const useCartStore = create<State>()((set, get) => ({
   cart: [],
+  // Methods
+  addProductToCart: (product) => {
+    const { cart } = get()
+
+    const productInCart = cart.some(
+      (item) => item.id === product.id && item.size === product.size,
+    )
+
+    if (!productInCart) {
+      set({ cart: [...cart, product] })
+      return
+    }
+
+    const updatedCartProducts = cart.map((item) =>
+      item.id === product.id && item.size === product.size
+        ? { ...item, quantity: item.quantity + product.quantity }
+        : item,
+    )
+    set({ cart: updatedCartProducts })
+  },
 }))
