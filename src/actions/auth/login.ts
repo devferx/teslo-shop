fcd40837e189fed21de154896e39c194aef3124d
devferx <ferkki@hotmail.com>
2024-04-18 18:41:53 -0400

    Refactor login functionality and UI

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -2,23 +2,27 @@
 
 import { AuthError } from 'next-auth'
 import { signIn } from '@/auth.config'
+import { sleep } from '@/utils'
 
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
 ) {
   try {
+    // await sleep(3)
     await signIn('credentials', formData)
-    return 'Success'
+    // return 'Success'
   } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.'
-        default:
-          return 'Something went wrong.'
-      }
-    }
+    // if (error instanceof AuthError) {
+    //   switch (error.type) {
+    //     case 'CredentialsSignin':
+    //       return 'Invalid credentials.'
+    //     default:
+    //       return 'Something went wrong.'
+    //   }
+    // }
+
+    return 'CredentialsSignin'
 
     throw error
   }
--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { useFormState } from 'react-dom'
+import { useFormState, useFormStatus } from 'react-dom'
 import Link from 'next/link'
 
 import { authenticate } from '@/actions'
+import { IoInformationOutline } from 'react-icons/io5'
+import clsx from 'clsx'
 
 export const LoginForm = () => {
   const [state, dispatch] = useFormState(authenticate, undefined)
@@ -25,9 +27,22 @@ export const LoginForm = () => {
         name="password"
       />
 
-      <button className="btn-primary" type="submit">
-        Ingresar
-      </button>
+      <div
+        className="flex h-8 items-end space-x-1"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {state === 'CredentialsSignin' && (
+          <div className="mb-2 flex flex-row">
+            <IoInformationOutline className="h-5 w-5 text-red-500" />
+            <p className="text-sm text-red-500">
+              Credenciales no son correctas
+            </p>
+          </div>
+        )}
+      </div>
+
+      <LoginButton />
 
       {/* divisor line */}
       <div className="my-5 flex items-center">
@@ -40,5 +55,18 @@ export const LoginForm = () => {
         Crear una nueva cuenta
       </Link>
     </form>
+  )
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <button
+      className={clsx({ 'btn-primary': !pending, 'btn-disabled': pending })}
+      type="submit"
+    >
+      Ingresar
+    </button>
   )
 }
