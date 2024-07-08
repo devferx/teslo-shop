e2f485ab3679c27c94f3c2e2b422ce2f0420392e
devferx <ferkki@hotmail.com>
2024-07-08 14:41:19 -0400

    Add Country model to schema and migration

--- scripts/seed-db.ts
+++ scripts/seed-db.ts
@@ -1,9 +1,12 @@
-import { initialData } from './seed'
 import prisma from '../src/lib/prisma'
+
+import { initialData } from './seed'
+import { countries } from './seed-countries'
 
 async function main() {
   console.log('Erasing database...')
   await prisma.user.deleteMany()
+  await prisma.country.deleteMany()
 
   await prisma.productImage.deleteMany()
   await prisma.product.deleteMany()
@@ -13,6 +16,10 @@ async function main() {
 
   await prisma.user.createMany({
     data: users,
+  })
+
+  await prisma.country.createMany({
+    data: countries,
   })
 
   console.log('Creating categories...')
