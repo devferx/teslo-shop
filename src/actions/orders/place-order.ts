28d8cdb4fd276690c13c0487b513a167248f5ed5
devferx <ferkki@hotmail.com>
2024-07-09 15:39:20 -0400

    Add functionality to calculate and store order total

--- src/actions/orders/place-order.ts
+++ src/actions/orders/place-order.ts
@@ -3,6 +3,7 @@
 import { auth } from '@/auth.config'
 
 import type { Address, Size } from '@/interfaces'
+import prisma from '@/lib/prisma'
 
 interface ProductToOrder {
   productId: string
@@ -17,6 +18,7 @@ export const placeOrder = async (
   const session = await auth()
   const userId = session?.user.id
 
+  // Validate if there is a user session
   if (!userId) {
     return {
       ok: false,
@@ -24,5 +26,37 @@ export const placeOrder = async (
     }
   }
 
-  console.log({ productIds, address, userId })
+  // Get products data
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((product) => product.productId),
+      },
+    },
+  })
+
+  // Calculate total
+  const itemsInOrder = productIds.reduce(
+    (count, product) => count + product.quantity,
+    0,
+  )
+
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity
+      const product = products.find((p) => p.id === item.productId)
+
+      if (!product) throw new Error('Product not found')
+
+      const subTotal = product.price * productQuantity
+      totals.subTotal += subTotal
+      totals.tax += subTotal * 0.15
+      totals.total += subTotal * 1.15
+
+      return totals
+    },
+    { subTotal: 0, tax: 0, total: 0 },
+  )
+
+  // Crear transacción
 }
