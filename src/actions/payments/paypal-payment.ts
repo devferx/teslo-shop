4856d54ef591a72823c02f94dbee0bd53172bc8c
devferx <ferkki@hotmail.com>
2024-07-12 17:45:25 -0400

    Add PayPal payment functionality

--- src/actions/payments/paypal-payment.ts
+++ src/actions/payments/paypal-payment.ts
@@ -1,8 +1,9 @@
 'use server'
 
+import type { PayPalOrderStatusResponse } from '@/interfaces'
+
 export const paypalCheckPayment = async (paypalTransactionId: string) => {
   const authToken = await getPayPalBearerToken()
-  console.log('🚀 ~ paypalCheckPayment ~ authToken:', authToken)
 
   if (!authToken) {
     return {
@@ -10,16 +11,35 @@ export const paypalCheckPayment = async (paypalTransactionId: string) => {
       message: 'No se pudo obtener el token de autenticación',
     }
   }
+
+  const resp = await verifyPayPalPayment(paypalTransactionId, authToken)
+
+  if (!resp) {
+    return {
+      ok: false,
+      message: 'No se pudo verificar el pago',
+    }
+  }
+
+  const { status, purchase_units } = resp
+
+  if (status !== 'COMPLETED') {
+    return {
+      ok: false,
+      message: 'Aun no se ha completado el pago',
+    }
+  }
+
+  // TODO Realiza la acutliazación de la orden en la base de datos
+  // const {  } = purse_units[0] // TODO: invoce ID
+
+  console.log({ status, purchase_units })
 }
 
 const getPayPalBearerToken = async (): Promise<string | null> => {
   const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID
   const PAYPAL_SECRET = process.env.PAYPAL_SECRET
   const oauth2Url = process.env.PAYPAL_OAUTH_URL as string
-
-  console.log('PAYPAL_CLIENT_ID', PAYPAL_CLIENT_ID)
-  console.log('PAYPAL_SECRET', PAYPAL_SECRET)
-  console.log('oauth2Url', oauth2Url)
 
   const base64Token = Buffer.from(
     `${PAYPAL_CLIENT_ID}:${PAYPAL_SECRET}`,
@@ -44,6 +64,33 @@ const getPayPalBearerToken = async (): Promise<string | null> => {
       r.json(),
     )
     return result.access_token as string
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+const verifyPayPalPayment = async (
+  paypalTransactionId: string,
+  bearerToken: string,
+): Promise<PayPalOrderStatusResponse | null> => {
+  const paypalOrderUrl = `${process.env.PAYPAL_ORDERS_URL}/${paypalTransactionId}`
+
+  const myHeaders = new Headers()
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
+  myHeaders.append('Authorization', `Bearer ${bearerToken}`)
+
+  const requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+  }
+
+  try {
+    const resp: PayPalOrderStatusResponse = await fetch(
+      paypalOrderUrl,
+      requestOptions,
+    ).then((r) => r.json())
+    return resp
   } catch (error) {
     console.log(error)
     return null
