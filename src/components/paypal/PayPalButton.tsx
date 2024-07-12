59928c07bf2477c5b6701e88e71250efb8e024a7
devferx <ferkki@hotmail.com>
2024-07-12 16:47:44 -0400

    Add skeleton to paypal buttons

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,7 +1,18 @@
 'use client'
 
-import { PayPalButtons } from '@paypal/react-paypal-js'
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
 
 export const PayPalButton = () => {
+  const [{ isPending }] = usePayPalScriptReducer()
+
+  if (isPending) {
+    return (
+      <div className="mb-16 animate-pulse text-center">
+        <div className="h-11 rounded bg-gray-300"></div>
+        <div className="mt-2 h-11 rounded bg-gray-300"></div>
+      </div>
+    )
+  }
+
   return <PayPalButtons />
 }
