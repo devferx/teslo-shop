b3e11c8e4d757afbbfdfdcc24a98853d4fa094b2
Fernando Quinteros Gutierrez <ferkki@hotmail.com>
2025-12-27 06:15:17 -0400

    style: update footer styling for improved layout and readability

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <footer className="mb-10 flex w-full justify-center text-xs">
+    <footer className="bg-black-1 flex w-full justify-center py-16 text-xs text-white">
       <Link href="/">
         <span className={`${titleFont.className} font-bold antialiased`}>
           Teslo
         </span>
-        <span>| shop </span>
+        <span> | shop </span>
         <span>&copy; {new Date().getFullYear()}</span>
       </Link>
 
